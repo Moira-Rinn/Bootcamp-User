e717e4a47397be8219de45b6ce3b6b3b1e9ffb9e
Sacha Moira Rinn <Moira.Rinn@gmail.com>
2021-08-12 00:53:41 -0400

    Final commit, finished assignment, added return self.

--- User.py
+++ User.py
@@ -8,9 +8,11 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdral(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
         return round(self.account_balance, 2)
@@ -18,6 +20,7 @@ class User:
     def transfer_money(self, other_user, amount):
         self.account_balance -= amount
         other_user.account_balance += amount
+        return self
 
 
 sara = User("Sara McCloud", 27, "July 19th, 1994", "SMcCloud@gmail.com", 1300)
@@ -26,22 +29,16 @@ rachael = User("Rachael Anderson", 19, "October 31st, 2002",
 liz = User("Elibeth McKnight", 43, "March 23rd, 1978",
            "EMcKnight@gmail.com", 10300)
 
-sara.make_deposit(657.39)
-sara.make_deposit(643.74)
-sara.make_withdral(173.27)
-sara.make_deposit(678.92)
+sara.make_deposit(657.39).make_deposit(
+    643.74).make_withdral(173.27).make_deposit(678.92)
 print(sara.display_user_balance())
 
 
-rachael.make_deposit(358.69)
-rachael.make_withdral(97.19)
-rachael.make_deposit(412.12)
-rachael.make_withdral(103.67)
+rachael.make_deposit(358.69).make_withdral(
+    97.19).make_deposit(412.12).make_withdral(103.67)
 print(rachael.display_user_balance())
 
-liz.make_deposit(1243.89)
-liz.make_withdral(872.09)
-liz.make_deposit(337.23)
+liz.make_deposit(1243.89).make_withdral(872.09).make_deposit(337.23)
 print(liz.display_user_balance())
 
 sara.transfer_money(liz, 100)
